9d2ab69a78ce0214b25f790d04bc4605c6787f69
lisilin013 <lisilin013@163.com>
2019-04-20 21:30:59 +0800

    update

--- train_test_split.py
+++ train_test_split.py
@@ -9,6 +9,8 @@ from sklearn.model_selection import train_test_split
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 BACKGROUND_IMG_PATH = os.path.join(BASE_DIR, 'task2img', 'backgroundimg')
 PORES_IMG_PATH = os.path.join(BASE_DIR, 'task2img', 'poresimg')
+TRAIN_IMG_PATH = os.path.join(BASE_DIR, 'train')
+TEST_IMG_PATH = os.path.join(BASE_DIR, 'test')
 
 background_imgs = os.listdir(BACKGROUND_IMG_PATH)
 pores_imgs = os.listdir(PORES_IMG_PATH)
@@ -44,9 +46,18 @@ y2 = [1] * len(pores_imgs)
 
 x_train1, x_test1, y_train1, y_test1 = train_test_split(x1, y1, test_size=0.2)
 x_train2, x_test2, y_train2, y_test2 = train_test_split(x2, y2, test_size=0.2)
-x_train = x_train1 + x_train2
+x_train = []
+x_test = []
+for img in x_train1:
+    x_train.append(os.path.join(BACKGROUND_IMG_PATH, img))
+for img in x_train2:
+    x_train.append(os.path.join(PORES_IMG_PATH, img))
+for img in x_test1:
+    x_test.append(os.path.join(BACKGROUND_IMG_PATH, img))
+for img in x_test2:
+    x_test.append(os.path.join(PORES_IMG_PATH, img))
+
 y_train = y_train1 + y_train2
-x_test = x_test1 + x_test2
 y_test = y_test1 + y_test2
 print ('x_train size = %d' % len(x_train))
 print ('y_train size = %d' % len(y_train))
@@ -55,8 +66,11 @@ print ('y_test size = %d' % len(y_test))
 print ('----------------end to split data set!----------------')
 print ('\n')
 
-# img_path = os.path.join(BACKGROUND_IMG_PATH, background_imgs[0])
-# img = cv2.imread(img_path)
+# write train images
+# for train_img_name, test_img_name in x_train, x_test:
+#     train_img_path = os.path.join(BACKGROUND_IMG_PATH, train_img_name)
+#     img = cv2.imread(train_img_path)
+#     cv2.imwrite(os.path.join(TRAIN_IMG_PATH, x_train[0]), img)
 # if img is not None:
 #     cv2.imshow('img', img)
 #     cv2.waitKey(0)
