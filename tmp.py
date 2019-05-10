8c95fdce30ada9555859b8c8d102b2d5fc176d00
lisilin013 <lisilin013@163.com>
2019-05-10 16:00:34 +0800

    tmp

--- tmp.py
+++ tmp.py
@@ -1,2 +1,10 @@
 # -*- coding:utf-8 -*-
-#!/usr/bin/python
+# !/usr/bin/python
+
+import pandas as pd
+import numpy as np
+
+import pandas as pd
+a = np.array([1, 2, 3, 1, 1, 2])
+result = np.sum(a==0)
+print result
